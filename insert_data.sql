e654ec93d9ea2e2b7647395d2ef4f402ed516519
Andy Samuel <samuela@wwu.edu>
2025-10-27 15:22:05 -0700

    Fixed a bug relating to string-values trying to be INSERTed into INT columns

--- insert_data.sql
+++ insert_data.sql
@@ -1,11 +1,11 @@
-INSERT INTO dbo.students(student_id, [name]) VALUES ('W123', 'Andy');
-INSERT INTO dbo.students(student_id, [name]) VALUES ('W456', 'Beth');
+INSERT INTO dbo.students(student_id, [name]) VALUES (123, 'Andy');
+INSERT INTO dbo.students(student_id, [name]) VALUES (456, 'Beth');
 
-INSERT INTO dbo.courses(course_id, title) VALUES ('111', 'Git Workshop');
-INSERT INTO dbo.courses(course_id, title) VALUES ('222', 'IPEDS 101');
-INSERT INTO dbo.courses(course_id, title) VALUES ('333', 'State and Federal Reporting For Dummies');
+INSERT INTO dbo.courses(course_id, title) VALUES (111, 'Git Workshop');
+INSERT INTO dbo.courses(course_id, title) VALUES (222, 'IPEDS 101');
+INSERT INTO dbo.courses(course_id, title) VALUES (333, 'State and Federal Reporting For Dummies');
 
-INSERT INTO dbo.course_enrollments(enrollment_id, course_id, student_id) VALUES (1, '111', 'W123');
-INSERT INTO dbo.course_enrollments(enrollment_id, course_id, student_id) VALUES (2, '111', 'W456');
-INSERT INTO dbo.course_enrollments(enrollment_id, course_id, student_id) VALUES (3, '222', 'W123');
-INSERT INTO dbo.course_enrollments(enrollment_id, course_id, student_id) VALUES (4, '333', 'W456');
+INSERT INTO dbo.course_enrollments(enrollment_id, course_id, student_id) VALUES (1, 111, 123);
+INSERT INTO dbo.course_enrollments(enrollment_id, course_id, student_id) VALUES (2, 111, 456);
+INSERT INTO dbo.course_enrollments(enrollment_id, course_id, student_id) VALUES (3, 222, 123);
+INSERT INTO dbo.course_enrollments(enrollment_id, course_id, student_id) VALUES (4, 333, 456);
